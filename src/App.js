fb2876bb5885e375381bbf98e371de810fbae0d3
伊豆原佑一 <izuhara_yuichi@Mame-PC.local>
2021-01-23 23:06:50 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { render } from "@testing-library/react";
 import React, { Component } from "react";
 
 // JSX
-function App() {
-  return (<h1>Hello, world!</h1>);
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}} />
+      </React.Fragment>
+    )
+  }
 }
-
-// Javascript
-// class App extends Component {
-//   render() {
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello, world!!"
-//     )
-//   }
-// }
 
 export default App;
