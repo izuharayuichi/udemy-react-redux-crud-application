470af91326733fe050791ff779beea125391fe8f
伊豆原佑一 <izuhara_yuichi@Mame-PC.local>
2021-01-23 22:44:13 +0900

    create why-JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
 
+// JSX
 function App() {
-  return (<div>Hello, world!</div>);
+  return (<h1>Hello, world!</h1>);
 }
+
+// Javascript
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world!!"
+//     )
+//   }
+// }
 
 export default App;
